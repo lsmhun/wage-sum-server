8941d9f0829a4f73110132f573310c2f056f3309
lsmhun <lsmlogin@gmail.com>
2022-12-14 13:21:32 +0100

    Bugfix - upsert and delete

--- internal/pkg/db/emp_repo.go
+++ internal/pkg/db/emp_repo.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"time"
+
 	openapi "github.com/lsmhun/wage-sum-server/internal/pkg/openapi"
 	"gorm.io/gorm"
 )
@@ -51,14 +53,34 @@ func (d *EmpDb) FindEmployeesByType(empType string) ([]openapi.Emp, error) {
 }
 
 func (d *EmpDb) CreateOrUpdateEmp(emp openapi.Emp) (openapi.Emp, error) {
-	result := d.db.FirstOrCreate(&emp, emp)
-	return emp, result.Error
+	var emp1, err1 = d.FindEmployeeById(emp.EmpId)
+	if err1 == nil {
+		var res1 = d.db.Model(&emp1).Updates(openapi.Emp{
+			FirstName: emp.FirstName,
+			LastName:  emp.LastName,
+			UserName:  emp.UserName,
+			Status:    emp.Status,
+			Type:      emp.Type,
+			MgrId:     emp.MgrId,
+		})
+		return emp1, res1.Error
+	} else {
+		var iid = d.makeTimestamp()
+		emp.EmpId = iid
+		emp.Id = iid
+		var res2 = d.db.FirstOrCreate(&emp, emp)
+		return emp, res2.Error
+	}
 }
 
 func (d *EmpDb) DeleteEmp(empId int64) (openapi.Emp, error) {
 	var emp1, err1 = d.FindEmployeeById(empId)
-	if err1 != nil {
+	if err1 == nil {
 		err1 = d.db.Delete(&emp1, emp1.Id).Error
 	}
 	return emp1, err1
+}
+
+func (d *EmpDb) makeTimestamp() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
 }
